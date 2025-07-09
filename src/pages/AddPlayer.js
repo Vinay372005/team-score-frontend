10230720642cc200d114c1914b2c1596bd321db7
Vinay372005 <ee24b006@iittp.ac.in>
2025-07-09 11:04:44 +0530

    Update AddPlayer.js

--- src/pages/AddPlayer.js
+++ src/pages/AddPlayer.js
@@ -1,7 +1,7 @@
 // src/pages/AddPlayer.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import '../FormStyle.css';
+import '../styles/Form.css';
 
 const AddPlayer = () => {
   const [form, setForm] = useState({ name: '', role: '', phone: '', photo: null });
