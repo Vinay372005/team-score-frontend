e3a2cd3e3ace1a2d23365b826c925950ea303cab
Vinay372005 <ee24b006@iittp.ac.in>
2025-07-09 11:17:11 +0530

    Update UpdateScore.js

--- src/pages/UpdateScore.js
+++ src/pages/UpdateScore.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { isAdmin } from '../utils/admin';
-import '../src/styles/Form.css';
+import '../styles/Form.css';
 
 const UpdateScore = () => {
   const [form, setForm] = useState({
