422c1db5eb20d62d542b81e925974a9c24768f0b
Vinay372005 <ee24b006@iittp.ac.in>
2025-07-09 12:02:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // ✅ keep this only if you created index.css
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
